d526fa5dff5c43404d1b9412e03e4adf0c169665
Yonro08 <quatroyondime@gmail.com>
2023-10-20 19:48:36 +0600

    add Bottom Icon

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -2,6 +2,8 @@ import { LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { BookmarkIcon } from "react-native-heroicons/solid";
+import { CloudIcon } from "react-native-heroicons/solid";
 
 import HomeScreenNavigation from "./HomeScreenNavigation";
 import HistoryScreenNavigation from "./HistoryScreenNavigation";
@@ -16,16 +18,24 @@ const AppNavigation = () => {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
           <Tab.Screen
             name="Home"
             component={HomeScreenNavigation}
-            options={{ headerShown: false }}
+            options={{
+              tabBarIcon: ({ color, size }) => {
+                return <CloudIcon size={size} color={color} />;
+              },
+            }}
           />
           <Tab.Screen
             name="History"
             component={HistoryScreenNavigation}
-            options={{ headerShown: false }}
+            options={{
+              tabBarIcon: ({ color, size }) => {
+                return <BookmarkIcon size={size} color={color} />;
+              },
+            }}
           />
         </Tab.Navigator>
       </NavigationContainer>
